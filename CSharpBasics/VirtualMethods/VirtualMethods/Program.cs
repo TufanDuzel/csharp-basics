7108ac0f5dc9430c47870bf9eb43bbde7e1fd133
Tufan Duzel <tufansahind@gmail.com>
2025-04-25 22:00:22 +0200

    CSharpBasics

---
+++ CSharpBasics/VirtualMethods/VirtualMethods/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SQLServer sqlserver = new SQLServer();
+            MySQL mysql = new MySQL();
+
+            sqlserver.Add();
+            mysql.Add();
+
+            Console.ReadLine();
+        }
+
+        class Database
+        {
+            public virtual void Add()
+            {
+                Console.WriteLine("Added by default.");
+            }
+        }
+
+        class SQLServer : Database
+        {
+            public override void Add()
+            {
+                Console.WriteLine("Added by SQL Server code.");
+                //base.Add();
+            }
+        }
+
+        class MySQL : Database
+        {
+            
+        }
+    }
+}
