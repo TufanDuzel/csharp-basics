291fcd2340753a5fd82ac99563c49eb4893d054e
Tufan Duzel <tufansahind@gmail.com>
2025-04-29 23:54:33 +0200

    CSharpBasics

---
+++ CSharpBasics/RecapDemo2/RecapDemo2/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecapDemo2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.logger = new DatabaseLogger();
+            customerManager.Add();
+
+            Console.ReadLine();
+        }
+
+        class CustomerManager
+        {
+            public ILogger logger { get; set; }
+            public void Add()
+            {
+                logger.Log();
+                Console.WriteLine("Customer added successfully.");
+            }
+        }
+
+        interface ILogger
+        {
+            void Log();
+        }
+
+        class DatabaseLogger : ILogger
+        {
+            public void Log()
+            {
+                Console.WriteLine("Logged to database.");
+            }
+        }
+
+        class EmailLogger : ILogger
+        {
+            public void Log()
+            {
+                Console.WriteLine("Logged to email.");
+            }
+        }
+
+        class SMSLogger : ILogger
+        {
+            public void Log()
+            {
+                Console.WriteLine("Logged via sms.");
+            }
+        }
+    }
+}
---
+++ CSharpBasics/Strings/Strings/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string city = "Istanbul";
+
+            foreach(var item in city)
+            {
+                Console.WriteLine(item);
+            }
+
+            string city2 = "Roma";
+
+            /*String cities = city + city2;
+            Console.WriteLine(cities);*/
+
+            Console.WriteLine(string.Format("{0} {1}", city, city2));
+
+
+
+            string sentence = "I love coding.";
+
+            var characters = sentence.Length;
+            Console.WriteLine(characters);
+            
+            var sentence2 = sentence.Clone();
+            Console.WriteLine(sentence2);
+
+            bool end = sentence.EndsWith(".");
+            Console.WriteLine(end);
+
+            bool start = sentence.StartsWith("I");
+            Console.WriteLine(start);
+
+            var index = sentence.IndexOf("coding");
+            Console.WriteLine(index);
+            // if the index can't be fined, the result will be "-1".
+
+            var indexBeginning = sentence.IndexOf(" ");
+            Console.WriteLine(indexBeginning);
+            // Starts searching the index from beginning.
+
+            var indexEnd = sentence.LastIndexOf(" ");
+            Console.WriteLine(indexEnd);
+            // Starts searching the index from end.
+
+            var adding = sentence.Insert(0, "Hi ");
+            Console.WriteLine(adding);
+
+            var cutting = sentence.Substring(7, 6);
+            Console.WriteLine(cutting);
+
+            var lower = sentence.ToLower();
+            Console.WriteLine(lower);
+
+            var upper = sentence.ToUpper();
+            Console.WriteLine(upper);
+
+            var replacing = sentence.Replace(" ", "- ");
+            Console.WriteLine(replacing);
+
+            var removing = sentence.Remove(6);
+            Console.WriteLine(removing);
+
+            Console.ReadLine();
+        }
+    }
+}
