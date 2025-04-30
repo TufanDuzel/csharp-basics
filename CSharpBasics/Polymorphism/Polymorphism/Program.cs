76be7c0592082b944bb9349f8523d95f400fc4fd
Tufan Duzel <tufansahind@gmail.com>
2025-04-30 21:32:45 +0200

    CSharpBasics

---
+++ CSharpBasics/Polymorphism/Polymorphism/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorphism
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ICustomerDal[] customerDals = new ICustomerDal[]
+            {
+                new SQLServerCustomerDal(),
+                new OracleCustomerDal(),
+                new MySQLCustomerDal()
+            };
+
+            foreach (var customerDal in customerDals)
+            {
+                customerDal.Add();
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
